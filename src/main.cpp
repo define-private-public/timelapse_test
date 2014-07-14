9c1104fecf64167efc2cef3979584df161ebc7d8
Ben <define-private-public@users.noreply.github.com>
2014-07-13 20:46:00 -0400

    Starting work on main executable

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+// Main source file for the timeline_test application
+
+// Includes
+#include <iostream>
+#include <opencv2/opencv.hpp>		// Using C++ version of OpenCV
+#include <cstdlib>
+#include <cstring>
+#include <csignal>
+#include <unistd.h>
+
+// Namespaces
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+	cout << "Capture a timelapse." << endl;
+
+	return 0;
+}
+
