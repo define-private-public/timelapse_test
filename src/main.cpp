484b77c58e76f7820f323338bbb920adfb019dfd
Ben <define-private-public@users.noreply.github.com>
2014-07-14 01:22:41 -0400

    Added directory checking.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,12 @@
 #include <cstdio>
 #include <cstring>
 #include <csignal>
+#include <cerrno>
+
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 
 // Definitions
 #define ARG_TIMEOUT 1
@@ -17,27 +22,26 @@
 #define MAX_HOURS 23
 #define MAX_MINUTES 59
 #define MAX_SECONDS 59
+#define RWX_PERMS 0777
+
 
 // Namespaces
 using namespace std;
 
 
 /*== Function prototypes ==*/
-inline void print_usage();		// Prints a usage message
 
 
 
 /*== Function definitions ==*/
-inline void print_usage() {
-	cout << "./timelapse_test <timeout> <duration> <destination>" << endl;
-}
+
 
 
 int main(int argc, char *argv[]) {
 	// Variables needed
 	unsigned long timeout, duration;
 	unsigned short durDays, durHours, durMinutes, durSeconds;
-	const char *destination;
+	const char *destDir;
 	int n;
 
 	// Welcome message
@@ -46,8 +50,8 @@ int main(int argc, char *argv[]) {
 	/*== Check the arguements ==*/
 	// Make sure we have enough arguments
 	if (argc < 4) {
-		cout << "Not enough arguements." << endl;
-		print_usage();
+		cout << "Not enough arguements, useage:" << endl;
+		cout << "  ./timelapse_test <timeout> <duration> <destination>" << endl;
 		exit(0);
 	}
 
@@ -59,28 +63,75 @@ int main(int argc, char *argv[]) {
 		timeout = atoi(argv[ARG_TIMEOUT]);
 
 	// Read the duration string
-	n = sscanf(argv[ARG_DURATION], "%u:%u:%u:%u", &durDays, &durHours, &durMinutes, &durSeconds);
+	n = sscanf(argv[ARG_DURATION], "%hu:%hu:%hu:%hu", &durDays, &durHours, &durMinutes, &durSeconds);
 	if (n < 4) {
 		// Make sure we read the correct amount, print an error otherwise
 		cerr << "\"" << argv[ARG_DURATION] << "\" is an invalid duration string, please use the format of:" << endl;
-		cerr << "\"DD:mm:hh:ss\"" << endl;
+		cerr << "  \"DD:mm:hh:ss\"" << endl;
 		exit(1);
 	} else if ((durDays > MAX_DAYS) || (durHours > MAX_HOURS) || (durMinutes > MAX_MINUTES) || (durSeconds > MAX_SECONDS)) {
 		cerr << "\"" << argv[ARG_DURATION] << "\" is an invalid duration string, please make sure to follow the limits of:" << endl;
-		cerr << "[0-" << MAX_DAYS << "] Days" << endl;
-		cerr << "[0-" << MAX_HOURS << "] Hours" << endl;
-		cerr << "[0-" << MAX_MINUTES << "] Minutes" << endl;
-		cerr << "[0-" << MAX_SECONDS << "] Seconds" << endl;
+		cerr << "  [0-" << MAX_DAYS << "] Days" << endl;
+		cerr << "  [0-" << MAX_HOURS << "] Hours" << endl;
+		cerr << "  [0-" << MAX_MINUTES << "] Minutes" << endl;
+		cerr << "  [0-" << MAX_SECONDS << "] Seconds" << endl;
+		exit(1);
+	} else if ((durDays + durHours + durMinutes + durSeconds) == 0) {
+		cerr << "Timelapse duration of 0 total seconds.  Are you serious?" << endl;
 		exit(1);
 	}
 
-	// TODO check directory
+	// Check the destination directory
+	destDir = argv[ARG_DESTINATION];
 
+	if (access(destDir, F_OK) == -1) {
+		// Destination dir doesn't exist, make it
+		cout << "\"" << destDir << "\" doesn't exist, making it." << endl;
+
+		// Make the directory
+		if (mkdir(destDir, RWX_PERMS) == -1) {
+			// Could not make the directoy, print an error message and exit
+			cerr << "Could not make the directory \"" << destDir << "\":" << endl;
+
+			// Pick a reason
+			switch (errno) {
+				case EACCES:
+					cerr << "  Could not write to the parent directory." << endl;
+					break;
+				case ENOENT:
+					cerr << "  Pathname doesn't exist." << endl;
+					break;
+				case EPERM:
+					cerr << "  Directory creation not permitted. Are you the parent's owner?" << endl;
+					break;
+				case EROFS:
+					cerr << "  Parent is a read-only filesystem." << endl;
+					break;
+				default:
+					cerr << "  Other error(" << errno << "): " << strerror(errno) << endl;
+			}
+
+			exit(1);
+		}
+
+	} else if (access(destDir, R_OK | W_OK | X_OK) == -1) {
+		// Cannot use the destination directory; exit
+		cerr << "Cannot use directory \"" << destDir << "\"." << endl;
+		cerr << "Please make sure that you can read, write, and execute it." << endl;
+		exit(1);
+	}
+
+
+
+
+	
+	// Stats
 	cout << "Timeout (seconds): " << timeout << endl;
 	cout << "Days: " << durDays << endl;
 	cout << "Hours: " << durHours << endl;
 	cout << "Minutes: " << durMinutes << endl;
 	cout << "Seconds: " << durSeconds << endl;
+	cout << "Destination Directory: " << destDir << endl;
 
 
 	return 0;
