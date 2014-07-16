509e73a798e143acdc4210f970490b30116a746f
Ben <define-private-public@users.noreply.github.com>
2014-07-16 19:41:02 -0400

    Added basic script

---
+++ src/mkfilm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# A small little utility script to compile the images to a movie using ffmpeg
+
+# Usage:
+#  ./mkfilm <images_directoy> <film_filename>
+#
+#  <images_directory> -- Where all the images from your timelapse are stored.
+#                        Will only look for .jpg images.
+#  <file_filename> -- Where to save the film to and what filename.
+#
+# Examples:
+#  ./mkfilm emerald/ emerald.mp4
+#  ./mkfilm sapphire/ sapphire.webm
+#
+#
+
+
