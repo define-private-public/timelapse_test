fdce25718df2df128566da806b7bb90873029185
Ben <define-private-public@users.noreply.github.com>
2014-07-16 21:26:22 -0400

    Setup associative array.

--- src/mkfilm.sh
+++ src/mkfilm.sh
@@ -14,4 +14,48 @@
 #
 #
 
+# Make sure we have enough arguemnts
+if [ $# -lt 2 ]; then
+	echo "Not enough arguemnts."
+	echo "Usage:"
+	echo "./mkfilm <images_directory> <film_filename>"
+	exit 0
+fi
+
+# Argument values
+imagesDir=$1
+filmName=$2
+jpgFiles=
+
+# Check that the directory exists and then do all of the re-naming
+if [ -d $imagesDir ]; then
+	# Directory is there, make sure that it has images
+	jpgFiles=$(ls $imagesDir/*.jpg)
+
+	if [ $? -eq 0 ]; then
+		# Figure out how many files we have to deal with
+		numFiles=($(ls -l $imagesDir/*.jpg | wc -l) - 1)
+		
+		# Rename the images
+		n=0
+		declare -A images
+		for f in $imagesDir/*.jpg; do
+			newId=$(printf "%0${#numFiles}d" $n)
+			images[$f]="$imagesDir/${newId}.jpg"
+			((n++))
+		done
+
+		for i in "${!images[@]}"; do
+			echo "$i -> ${images[$i]}"
+		done
+
+	else
+		# Was not able to find a single jpeg, just exit
+		echo "Couldn't find .jpg files"
+	fi
+else
+	# No directory, just exit
+	echo "\"$imagesDir\" is not a valid directory."
+	exit 1
+fi
 
